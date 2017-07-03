7d9dd84a2c67a73f6c4dab2c4dbc46634053ce27
molee1905 <mr.lee1905@gmail.com>
2017-07-04 01:13:52 +0800

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+// app.use(function (req, res, next) {
+//     req.on('data', function (chunk) {
+//         console.log(JSON.parse(chunk.toString()))
+//     });
+//     next();
+// });
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.use(bodyParser.json());
+
+app.post('/payload', function (req, res) {
+    console.log(new Date(), req.body.ref)
+    res.send('ok');
+});
+
+app.listen(8088, function () {
+    console.log('listening on port 8088!');
+});
